8c805adb4bad8159d8e465017752608306de9d78
xRain <xrain@simcu.com>
2021-01-07 02:22:27 +0800

    first commit

---
+++ Application.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using SimApi.Communications;
+using Newtonsoft.Json;
+using SimApi.Helpers;
+
+namespace SimUcApp
+{
+    public class Application
+    {
+        private string Server { get; }
+        public string AppId { get; }
+        private string AppKey { get; }
+
+        public Application(string server, string appId, string appKey)
+        {
+            Server = server + "/api/application";
+            AppId = appId;
+            AppKey = appKey;
+        }
+
+        private string Query(string uri, string body)
+        {
+            var wc = new WebClient();
+            wc.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+            Console.WriteLine($"请求的地址是: {Server + uri}");
+            Console.WriteLine($"请求的内容: {body}");
+            var resp = wc.UploadString(Server + uri, body);
+            Console.WriteLine($"请求的结果: {resp}");
+            return resp;
+        }
+
+        private string SignRequest<T>(T request)
+        {
+            (request as SimUcRequest.BaseApiRequest).AppId = AppId;
+            (request as SimUcRequest.BaseApiRequest).Time = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds;
+            var requestJson = JsonConvert.SerializeObject(request);
+            var parameters = JsonDocument.Parse(requestJson).RootElement.EnumerateObject();
+
+            var dic = parameters.ToDictionary(x => x.Name, x => x.Value.ToString());
+            dic.Remove("Sign");
+            var sorted = dic.OrderBy(x => x.Key);
+            string signStr = "";
+            foreach (var item in sorted)
+            {
+                signStr += $"{item.Key}={item.Value}&";
+            }
+
+            signStr = signStr.TrimEnd('&');
+            Console.WriteLine($"签名的字符串: {signStr}");
+            var sign = SimApiUtil.Md5(signStr + AppKey);
+            Console.WriteLine($"签名: {sign}");
+            (request as SimUcRequest.BaseApiRequest).Sign = sign;
+            return JsonConvert.SerializeObject(request);
+        }
+
+        /// <summary>
+        /// 更新应用卡片
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="groupId"></param>
+        /// <param name="type"></param>
+        /// <param name="data"></param>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public SimApiBaseResponse UpdateCard(int userId, int groupId, CardDataTypeEnum type, string data, string image = null)
+        {
+            var request = new SimUcRequest.UpdateCardRequest
+            {
+                UserId = userId,
+                GroupId = groupId,
+                Type = type.ToString(),
+                Data = data,
+                Image = image
+            };
+            var body = SignRequest(request);
+            Console.WriteLine(body);
+            var resp = Query("/card", body);
+            return JsonConvert.DeserializeObject<SimApiBaseResponse>(resp);
+        }
+
+
+        /// <summary>
+        /// 向用户发送信息
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="type"></param>
+        /// <param name="text"></param>
+        /// <param name="title"></param>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public SimApiBaseResponse SendMessage(int userId, CardDataTypeEnum type, string text, string title = null, string image = null)
+        {
+            var request = new SimUcRequest.SendMessageRequest
+            {
+                UserId = userId,
+                Type = type.ToString(),
+                Text = text,
+                Title = title,
+                Image = image
+            };
+            var body = SignRequest(request);
+            var resp = Query("/message", body);
+            return JsonConvert.DeserializeObject<SimApiBaseResponse>(resp);
+        }
+
+        /// <summary>
+        /// 使用Token获取用户信息
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public SimUcResponse.GetUserByTokenResponse GetUserByToken(string token)
+        {
+            var request = new SimUcRequest.GetUserByTokenRequest
+            {
+                Token = token
+            };
+            var body = SignRequest(request);
+            var resp = Query("/token", body);
+            return JsonConvert.DeserializeObject<SimUcResponse.GetUserByTokenResponse>(resp);
+        }
+
+        /// <summary>
+        /// 发起交易
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="name"></param>
+        /// <param name="ext"></param>
+        /// <returns></returns>
+        public SimApiBaseResponse<string> TradeCreate(int amount, string name, string ext)
+        {
+            var request = new SimUcRequest.TradeCreateRequest
+            {
+                Amount = amount,
+                Name = name,
+                Ext = ext
+            };
+            var body = SignRequest(request);
+            var resp = Query("/trade/create", body);
+            return JsonConvert.DeserializeObject<SimApiBaseResponse<string>>(resp);
+        }
+
+        /// <summary>
+        /// 查询交易
+        /// </summary>
+        /// <param name="tradeNo"></param>
+        /// <returns></returns>
+        public SimUcResponse.TradeCheckResponse TradeCheck(string tradeNo)
+        {
+
+            var request = new SimUcRequest.TradeCheckRequest
+            {
+                TradeNo = tradeNo
+            };
+            var body = SignRequest(request);
+            var resp = Query("/trade/result", body);
+            return JsonConvert.DeserializeObject<SimUcResponse.TradeCheckResponse>(resp);
+        }
+    }
+
+}
---
+++ Request.cs
@@ -0,0 +1,84 @@
+﻿using System;
+namespace SimUcApp
+{
+    public class SimUcRequest
+    {
+        public class BaseApiRequest
+        {
+            public string AppId { get; set; }
+            public int Time { get; set; }
+            public string Sign { get; set; }
+        }
+
+        public class UpdateCardRequest : BaseApiRequest
+        {
+            public int GroupId { get; set; }
+            public int UserId { get; set; }
+
+            /// <summary>
+            /// 数据类型，只可以为 image，text，html
+            /// </summary>
+            public string Type { get; set; }
+
+            /// <summary>
+            /// type为image的时候，这里是配文，为text的时候，这里是文字，为html的时候，这里是html链接
+            /// </summary>
+            public string Data { get; set; }
+
+            /// <summary>
+            /// 只有type为image的时候，这里是图片链接
+            /// </summary>
+            public string Image { get; set; }
+        }
+
+
+        public class SendMessageRequest : BaseApiRequest
+        {
+            public int UserId { get; set; }
+
+            /// <summary>
+            /// 数据类型，只可以为 image，text，html
+            /// </summary>
+            public string Type { get; set; }
+
+            /// <summary>
+            /// 可不填写
+            /// </summary>
+            public string Title { get; set; }
+
+            /// <summary>
+            /// type为image的时候，这里是配文，为text的时候，这里是文字，为html的时候，这里是html链接
+            /// </summary>
+            public string Text { get; set; }
+
+            /// <summary>
+            /// 只有type为image的时候，这里是图片链接
+            /// </summary>
+            public string Image { get; set; }
+        }
+
+        public class GetUserByTokenRequest : BaseApiRequest
+        {
+            public string Token { get; set; }
+        }
+
+        public class TradeCreateRequest : BaseApiRequest
+        {
+            public int Amount { get; set; }
+            public string Name { get; set; }
+            public string Ext { get; set; }
+        }
+
+        public class TradeCheckRequest : BaseApiRequest
+        {
+            public string TradeNo { get; set; }
+        }
+    }
+
+    public enum CardDataTypeEnum
+    {
+        Text,
+        Image,
+        Html
+    }
+}
